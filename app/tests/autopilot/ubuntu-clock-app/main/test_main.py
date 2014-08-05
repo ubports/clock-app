2324c8df1b90dd2d5d2e3a04e5fbd97f75dd3062
Daniel Holbach <daniel.holbach@canonical.com>
2014-08-05 09:29:43 +0200

    Revert 'Rename 'ubuntu-clock-app' to 'ubuntu_clock_app' in tests.'

--- app/tests/autopilot/ubuntu-clock-app/main/test_main.py
+++ app/tests/autopilot/ubuntu-clock-app/main/test_main.py
@@ -7,13 +7,13 @@ from textwrap import dedent
 from testtools.matchers import Is, Not, Equals
 from testtools import skip
 import os
-from ubuntu_clock_app import UbuntuTouchAppTestCase
+from ubuntu-clock-app import UbuntuTouchAppTestCase
 
 
 class MainTests(UbuntuTouchAppTestCase):
     """Generic tests for the Hello World"""
 
-    test_qml_file = "%s/%s.qml" % (os.path.dirname(os.path.realpath(__file__)),"../../../../ubuntu_clock_app")
+    test_qml_file = "%s/%s.qml" % (os.path.dirname(os.path.realpath(__file__)),"../../../../ubuntu-clock-app")
 
     def test_0_can_select_mainView(self):
         """Must be able to select the mainview."""
