849a96568c25328570561aca23feac2a42d91a55
Brendan Donegan <brendan.donegan@canonical.com>
2014-11-27 08:12:12 +0000

    Fix copyright header and add self parameter to function in fixture_setup

--- tests/autopilot/ubuntu_clock_app/fixture_setup.py
+++ tests/autopilot/ubuntu_clock_app/fixture_setup.py
@@ -1,9 +1,18 @@
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
-# Copyright 2014 Canonical
+# Copyright (C) 2014 Canonical Ltd
 #
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License version 3, as published
-# by the Free Software Foundation.
+# This file is part of Ubuntu Clock App
+#
+# Ubuntu Clock App is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# Ubuntu Clock App is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Clock app autopilot fixtures."""
 
@@ -19,7 +28,7 @@ class LocationServiceTestEnvironment(fixtures.Fixture):
         self._set_location_service_testing(True)
         self.addCleanup(self._set_location_service_testing(False))
 
-    def _set_location_service_testing(test_mode):
+    def _set_location_service_testing(self, test_mode):
         test = 'true' if test_mode else 'false'
         try:
             subprocess.check_call(
