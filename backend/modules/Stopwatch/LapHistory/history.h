100e550459df85d97249ab927dd5ce537d7db43d
Nekhelesh Ramananthan <krnekhelesh@gmail.com>
2015-08-24 11:52:57 +0200

    revert copyright header change in stopwatch laphistory plugin

--- backend/modules/Stopwatch/LapHistory/history.h
+++ backend/modules/Stopwatch/LapHistory/history.h
@@ -1,20 +1,22 @@
-/*
- * Copyright (C) 2015 Canonical Ltd
- *
- * This file is part of Ubuntu Clock App
- *
- * Ubuntu Clock App is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
- * Ubuntu Clock App is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*****************************************************************************
+ * Copyright: 2015 Michael Zanetti <michael_zanetti@gmx.net>                 *
+ *                                                                           *
+ * This file is part of ubuntu-authenticator                                 *
+ *                                                                           *
+ * This prject is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation, either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This project is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.     *
+ *                                                                           *
+ ****************************************************************************/
 
 #ifndef HISTORY_H
 #define HISTORY_H
